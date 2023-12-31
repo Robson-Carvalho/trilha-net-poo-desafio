035109a45b9fbc21609a27c80875775a747053f1
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-12-31 14:31:25 -0300

    feat: adiciona regras e validações

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+
+Nokia smartphoneNokia = new Nokia("22355", "L43", "BR", 3000);
+smartphoneNokia.InstalarAplicativo("Whatsapp");
+smartphoneNokia.ReceberLigacao();
+smartphoneNokia.Ligar();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Iphone smartphoneApple = new Iphone("2235e", "10", "BR", 3500);
+smartphoneApple.InstalarAplicativo("Whatsapp");
+smartphoneApple.ReceberLigacao();
+smartphoneApple.Ligar();
+
